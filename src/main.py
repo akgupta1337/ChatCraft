88fd43c6d1b8b0c3686ab4e261fc2fd17a9f99b0
Akhil Kumar Gupta <138664640+akgupta1337@users.noreply.github.com>
2023-12-19 17:42:12 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,14 @@ from ChatCraft import ChatCraft
 
 
 def main():
+    """
+    Before starting I suggest you to create a new profile in chrome and login with whatsapp in that,
+    then right click on the new chrome shorcut of your profile > show properties and in the Target section 
+    at the end you will see something like Profile 2 or whatever number, put that here since it will sign you
+    out while opening the browser.
+
+    """
+    
     test_bot = ChatCraft(profile="Profile X", user_name="your user name")
 
     test_bot.start(chat_name="exact name of chat you want to deploy this bot in")
