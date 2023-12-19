0e0e7cd238d8a1ac6e6924ed079a5409d54ea476
Akhil Kumar Gupta <138664640+akgupta1337@users.noreply.github.com>
2023-12-19 15:37:03 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from src.ChatCraft import ChatCraft
 
 
 def main():
-    test_bot = ChatCraft(profile="Profile 10", user_name="akgup")
+    test_bot = ChatCraft(profile="Profile X", user_name="your user name")
 
-    test_bot.start(chat_name="Bot Testing")
+    test_bot.start(chat_name="exact name of chat you want to deploy this bot in")
 
 
 if __name__ == "__main__":
